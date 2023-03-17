8a73c0bef3ae589285bee78ebcf7e5e90ac22f0f
lianwu <lianwu930@126.com>
2023-03-18 00:42:28 +0800

    提交 AnnotationUtil#getAnnotatedFields

--- mybatis-ext/src/main/java/cn/doocom/mybatis/ext/util/AnnotationUtil.java
+++ mybatis-ext/src/main/java/cn/doocom/mybatis/ext/util/AnnotationUtil.java
@@ -1,8 +1,47 @@
 package cn.doocom.mybatis.ext.util;
 
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
 
 public class AnnotationUtil {
 
+    public static List<Field> getAnnotatedFields(Class<?> clz, Class<? extends Annotation> annotation) {
+        return getAnnotatedFields(clz, annotation, false);
+    }
 
+    public static List<Field> getAnnotatedFields(Class<?> clz, Class<? extends Annotation> annotation, boolean withSuperClass) {
+        List<Field> res = new ArrayList<>();
+        if (withSuperClass)
+            doGetAnnotatedFieldsWithSuperClass(clz, annotation, res);
+        else
+            doGetAnnotatedFields(clz, annotation, res);
+        return res;
+    }
+
+    private static void doGetAnnotatedFieldsWithSuperClass(Class<?> clz, Class<? extends Annotation> annotation, List<Field> res) {
+        Class<?> superClz;
+        if (Object.class == clz || (superClz = clz.getSuperclass()) == null)
+            return ;
+        doGetAnnotatedFieldsWithSuperClass(superClz, annotation, res);
+        Field[] declaredFields = clz.getDeclaredFields();
+        for (Field field : declaredFields) {
+            if (field.isAnnotationPresent(annotation)) {
+                res.add(field);
+            }
+        }
+    }
+
+    private static void doGetAnnotatedFields(Class<?> clz, Class<? extends Annotation> annotation, List<Field> res) {
+        if (Object.class == clz || clz.getSuperclass() == null)
+            return ;
+        Field[] declaredFields = clz.getDeclaredFields();
+        for (Field field : declaredFields) {
+            if (field.isAnnotationPresent(annotation)) {
+                res.add(field);
+            }
+        }
+    }
 
 }
