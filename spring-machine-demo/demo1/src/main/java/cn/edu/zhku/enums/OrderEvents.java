aa533b57eaeab227670daa720ccc2d1bc80d197d
lianwu930 <60196982+jintao990930@users.noreply.github.com>
2023-03-28 00:20:40 +0800

    Delete spring-machine-demo directory

--- spring-machine-demo/demo1/src/main/java/cn/edu/zhku/enums/OrderEvents.java
+++
@@ -1,8 +0,0 @@
-package cn.edu.zhku.enums;
-
-public enum OrderEvents {
-
-    PAY,  // 支付
-    RECEIVE,  // 收货
-
-}
--- spring-machine-demo/demo1/src/main/java/cn/edu/zhku/enums/OrderStates.java
+++
@@ -1,9 +0,0 @@
-package cn.edu.zhku.enums;
-
-public enum OrderStates {
-
-    UNPAID,  // 待支付
-    WAITING_FOR_RECEIVE,  // 待收货
-    DONE,  // 结束
-
-}
